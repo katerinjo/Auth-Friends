bab3dee546ee66ceb7dbb848baa7d63387c1e101
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-08-20 15:21:05 -0700

    navlink

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import axios from 'axios';
 import './App.css';
 
@@ -16,10 +17,19 @@ function App() {
   }, [pending]);
 
   return (
-    <div className='App'>
-      <FriendForm subFun={setPending} />
-      <ItemList items={friends} failureText='you have no friends'/>
-    </div>
+    <Router>
+      <div className='App'>
+        <h1>Friends List App</h1>
+        <NavLink to='/list'>access friends</NavLink>
+        <Route path='/' exact component={} />
+        <Route path='/list' render={() => (
+          <>
+            <FriendForm subFun={setPending} />
+            <ItemList items={friends} failureText='you have no friends' />
+          </>
+        )} />
+      </div>
+    </Router>
   );
 }
 
